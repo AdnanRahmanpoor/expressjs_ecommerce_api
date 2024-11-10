0c6e51d2a927c3ff718a910fd5e6705ada7ecbad
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-11-11 02:47:46 +0400

    product update and delete routes and operations

--- controllers/productController.js
+++ controllers/productController.js
@@ -63,3 +63,32 @@ exports.getProducts = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+// Update a product
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' })
+        }
+        res.json(product);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Delete a product
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found'})
+        }
+        res.json({ messgae: "Product deleted" });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 // Routes which will be used by api
 
 const express = require('express');
-const { createProduct, getProduct, getProducts } = require("../controllers/productController");
+const { createProduct, getProduct, getProducts,updateProduct, deleteProduct } = require("../controllers/productController");
 const router = express.Router();
 
 // post method to create a new product
@@ -12,5 +12,11 @@ router.get('/:id', getProduct)
 
 // get method to get all products
 router.get('/', getProducts)
+
+// put method to update a product
+router.put('/:id', updateProduct);
+
+// delete method to delete a product
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
